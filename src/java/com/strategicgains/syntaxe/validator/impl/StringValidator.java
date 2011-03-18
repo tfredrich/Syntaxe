c5c989d4f4dc97665b9a0dcd71f017340b7ed8a1
Todd Fredrich <tfredrich@gmail.com>
2011-03-18 13:36:24 -0600

    Renamed BasicValidate to StringValidation

---
+++ src/java/com/strategicgains/syntaxe/validator/impl/StringValidator.java
@@ -0,0 +1,73 @@
+/*
+    Copyright 2011, Strategic Gains, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+package com.strategicgains.syntaxe.validator.impl;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import com.strategicgains.syntaxe.annotation.StringValidation;
+import com.strategicgains.syntaxe.util.Validations;
+import com.strategicgains.syntaxe.validator.AnnotatedFieldValidator;
+
+/**
+ * @author toddf
+ * @since Mar 17, 2011
+ */
+public class StringValidator
+extends AnnotatedFieldValidator<StringValidation>
+{
+	/**
+     * @param field
+     * @param annotation
+     */
+    public StringValidator(Field field, StringValidation annotation)
+    {
+	    super(field, annotation);
+    }
+
+    @Override
+    public void perform(Object instance, List<String> errors)
+    {
+		String name = determineName();
+		Object value = getValue(instance);
+
+		if (getAnnotation().required())
+		{
+			String stringValue = (value == null ? null : String.valueOf(value));
+			Validations.require(name, stringValue, errors);
+		}
+
+		if (getAnnotation().minLength() > 0)
+		{
+			String stringValue = (value == null ? null : String.valueOf(value));
+			Validations.minLength(name, stringValue, getAnnotation().minLength(), errors);
+		}
+
+		if (getAnnotation().maxLength() > 0)
+		{
+			String stringValue = (value == null ? null : String.valueOf(value));
+			Validations.maxLength(name, stringValue, getAnnotation().maxLength(), errors);
+		}
+    }
+
+	/**
+     * @return
+     */
+    private String determineName()
+    {
+		return (getAnnotation().name().isEmpty() ? getFieldName() : getAnnotation().name());
+    }
+}
