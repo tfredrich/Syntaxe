9f31ee9ec51dc9948ac46a005ead3c0d392ebc87
Todd Fredrich <tfredrich@gmail.com>
2014-03-17 16:13:08 -0600

    Introduced LongValidation annotation per issue #2

---
+++ src/java/com/strategicgains/syntaxe/validator/impl/LongValidator.java
@@ -0,0 +1,74 @@
+/*
+    Copyright 2011, Strategic Gains, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+package com.strategicgains.syntaxe.validator.impl;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import com.strategicgains.syntaxe.annotation.IntegerValidation;
+import com.strategicgains.syntaxe.annotation.LongValidation;
+import com.strategicgains.syntaxe.util.Validations;
+import com.strategicgains.syntaxe.validator.AnnotatedFieldValidator;
+
+/**
+ * @author toddf
+ * @since Mar 17, 2011
+ */
+public class LongValidator
+extends AnnotatedFieldValidator<LongValidation>
+{
+	/**
+     * @param field
+     * @param annotation
+     */
+    public LongValidator(Field field, LongValidation annotation)
+    {
+	    super(field, annotation);
+    }
+
+    @Override
+    public void perform(Object instance, List<String> errors)
+    {
+		String name = determineName();
+		Object value = getValue(instance);
+		
+		if (isArray())
+		{
+			// TODO: validate the elements in the array.
+			throw new UnsupportedOperationException("Validating an array of Long instances is not currently supported");
+		}
+
+		if (getAnnotation().min() != Long.MIN_VALUE)
+		{
+			long actual = ((Long) value).longValue();
+			Validations.greaterThanOrEqual(name, actual, getAnnotation().min(), errors);
+		}
+
+		if (getAnnotation().max() != Long.MAX_VALUE)
+		{
+			long actual = ((Long) value).longValue();
+			Validations.lessThanOrEqual(name, actual, getAnnotation().max(), errors);
+		}
+    }
+
+	/**
+     * @return
+     */
+    private String determineName()
+    {
+		return (getAnnotation().name().isEmpty() ? getFieldName() : getAnnotation().name());
+    }
+}
---
+++ test/java/com/strategicgains/syntaxe/AbstractValidatableTest.java
@@ -0,0 +1,96 @@
+package com.strategicgains.syntaxe;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.strategicgains.syntaxe.annotation.Required;
+
+public class AbstractValidatableTest
+{
+	@Test
+	public void shouldValidateExtenderViaEngine()
+	{
+		List<String> errors = ValidationEngine.validate(new Inner());
+		assertEquals(2, errors.size());
+		assertTrue(errors.contains("'other' field is required"));
+		assertTrue(errors.contains("String Value is required"));
+	}
+
+	@Test
+	public void shouldValidateExtenderViaValidate()
+	{
+		try
+		{
+			new Inner().validate();
+		}
+		catch(ValidationException v)
+		{
+			assertEquals("String Value is required, 'other' field is required", v.getMessage());
+		}
+	}
+	@Test
+	public void shouldValidateImplViaEngine()
+	{
+		List<String> errors = ValidationEngine.validate(new InnerImpl());
+		assertEquals(2, errors.size());
+		assertTrue(errors.contains("'other' field is required"));
+		assertTrue(errors.contains("String Value is required"));
+	}
+
+	@Test
+	public void shouldValidateImplViaValidate()
+	{
+		try
+		{
+			new InnerImpl().validate();
+		}
+		catch(ValidationException v)
+		{
+			assertEquals("'other' field is required, String Value is required", v.getMessage());
+		}
+	}
+
+	public class Inner
+	extends AbstractValidatable
+	{
+		@Required("String Value")
+		private String string;
+
+		private String other;
+
+		@Override
+		protected void validateInto(List<String> errors)
+		{
+			if (other == null)
+			{
+				errors.add("'other' field is required");
+			}
+		}
+	}
+
+	public class InnerImpl
+	implements Validatable
+	{
+		@Required("String Value")
+		private String string;
+
+		private String other;
+
+		@Override
+		public void validate()
+		{
+			List<String> errors = ValidationEngine.validate(this);
+
+			if (other == null)
+			{
+				errors.add("'other' field is required");
+			}
+
+			throw new ValidationException(errors);
+		}
+	}
+}
